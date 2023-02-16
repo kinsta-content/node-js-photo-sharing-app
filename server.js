b55e4d85600c16b02d9b2b3b36926901f6cfe54a
BBWeb3 <brendan.berkenshire@gmail.com>
2023-02-16 19:48:03 +1300

    EJS Intro and Templating

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const { response } = require('express');
 const express = require('express');
 const app = new express();
 const port = 8080;
-app.use(express.static("public"));
+app.use(express.static('public'))
+app.set("view engine", "ejs")
 app.listen(port, ()=>{
     console.log(`Serving photo app on http://localhost:${port}`);
+})
+
+app.get("/", (request, response) =>{
+    response.render("index");
 })
